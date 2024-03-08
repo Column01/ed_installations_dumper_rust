7aaff01656393a1b3317b98deb84f81f18e353a3
Column01 <Column01Dev@gmail.com>
2024-03-08 14:51:08 -0500

    Change import code to move files after processing

--- src/importer.rs
+++ src/importer.rs
@@ -4,6 +4,7 @@ use rayon::prelude::*;
 use serde_json::Value;
 use std::fs::File;
 use std::io::{BufRead, BufReader, Error};
+use std::path::Path;
 
 enum Reader {
     DecompressorReader(BufReader<BzDecoder<File>>),
@@ -22,7 +23,7 @@ fn get_reader(file_name: &str) -> Result<Reader, Error> {
 
 pub fn import_files(
     client: &Client,
-    file_names: &Vec<String>,
+    file_paths: &Vec<String>,
     num_workers: usize,
 ) -> std::io::Result<()> {
     let pool = rayon::ThreadPoolBuilder::new()
@@ -30,38 +31,62 @@ pub fn import_files(
         .build()
         .unwrap();
     pool.install(|| {
-        file_names.par_iter().for_each(|file_name| {
-            println!("Importing file: {}", file_name);
+        file_paths.par_iter().for_each(|file_path| {
+            println!("Importing file: {}", file_path);
             // Get the reader for the file and import it
             let db = client.database("FSSSignalDiscovered");
             let collection = db.collection("rust_test");
 
-            match get_reader(file_name) {
+            match get_reader(file_path) {
                 Ok(Reader::DecompressorReader(r)) => {
                     // Compressed file reader
+                    // Collect all lines from the file and store them
                     let mut lines = Vec::new();
                     for line in r.lines() {
                         let line = line.unwrap();
                         let json_blob: Value = serde_json::from_str(&line)
-                            .expect(&format!("Error loading json data from: {}", file_name));
+                            .expect(&format!("Error loading json data from: {}", file_path));
                         let doc = bson::to_document(&json_blob)
                             .expect("Error converting json blob to bson!");
                         lines.push(doc);
                     }
+                    // Insert all lines into the collection
                     let _ = collection.insert_many(lines, None).unwrap();
+
+                    // Move the file after processing
+                    let file_name = file_path.replace("downloads/", "");
+                    let processed_file_path = format!("downloads/processed/{}", file_name);
+                    // Ensure the processed directory exists
+                    if !Path::new("downloads/processed").exists() {
+                        std::fs::create_dir_all("downloads/processed").unwrap();
+                    }
+                    println!("{} => {}", file_path, processed_file_path);
+                    std::fs::rename(file_path, processed_file_path).expect("Error moving file after import!");
                 }
                 Ok(Reader::NormalReader(r)) => {
                     // Normal file reader
+                    // Collect all lines from the file and store them
                     let mut lines = Vec::new();
                     for line in r.lines() {
                         let line = line.unwrap();
                         let json_blob: Value = serde_json::from_str(&line)
-                            .expect(&format!("Error loading json data from: {}", file_name));
+                            .expect(&format!("Error loading json data from: {}", file_path));
                         let doc = bson::to_document(&json_blob)
                             .expect("Error converting json blob to bson!");
                         lines.push(doc);
                     }
+                    // Insert all lines into the collection
                     let _ = collection.insert_many(lines, None).unwrap();
+
+                    // Move the file after processing
+                    let file_name = file_path.replace("downloads/", "");
+                    let processed_file_path = format!("downloads/processed/{}", file_name);
+                    // Ensure the processed directory exists
+                    if !Path::new("downloads/processed").exists() {
+                        std::fs::create_dir_all("downloads/processed").unwrap();
+                    }
+                    println!("{} => {}", file_path, processed_file_path);
+                    std::fs::rename(file_path, processed_file_path).expect("Error moving file after import!");
                 }
                 Err(_) => todo!(),
             }
